d21204f73711b85fde6150b1d540c01132fb5ee1
dydeepak97 <dydeepak97@example.com>
2018-01-30 00:45:14 +0530

    Initial commit

---
+++ app/src/main/java/com/ilusons/silence/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ilusons.silence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
