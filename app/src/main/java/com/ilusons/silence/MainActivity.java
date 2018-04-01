e1dad7a3ebb29039797af3046be9770e4adc98dd
dydeepak97 <dydeepak97@example.com>
2018-04-01 19:46:48 +0530

    Layout Work: Drawer Fixed

--- app/src/main/java/com/ilusons/silence/MainActivity.java
+++ app/src/main/java/com/ilusons/silence/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private android.support.v7.widget.Toolbar mToolBar;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
+        mToolBar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolBar) ;
+        setSupportActionBar(mToolBar);
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_main);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
