3e1999062799df6e6a9a3986f06f43992f66998d
dydeepak97 <dydeepak97@example.com>
2018-04-08 02:06:07 +0530

    Layout Work: Chat UI

--- app/src/main/java/com/ilusons/silence/ConversationActivity.java
+++ app/src/main/java/com/ilusons/silence/ConversationActivity.java
@@ -1,19 +1,39 @@
 package com.ilusons.silence;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.ilusons.silence.data.User;
+import com.ilusons.silence.views.ConversationsFragment;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ConversationActivity extends AppCompatActivity {
 
     Toolbar toolbar;
 
+    RecyclerView rc_view;
+    ChatAdaptor chatAdaptor;
 
-    @Override
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
@@ -21,6 +41,19 @@ public class ConversationActivity extends AppCompatActivity {
         toolbar = (Toolbar)findViewById(R.id.toolBarChat);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setIcon(R.drawable.user_avatar_256);
+        getSupportActionBar().setTitle("Username");
+
+        rc_view= (RecyclerView) findViewById(R.id.rv_message_list);
+       LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+       rc_view.setLayoutManager(layoutManager);
+
+       rc_view.setHasFixedSize(true);
+
+       chatAdaptor = new ChatAdaptor(this);
+
+       rc_view.setAdapter(chatAdaptor);
 
     }
 
@@ -56,4 +89,60 @@ public class ConversationActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static class ChatAdaptor extends RecyclerView.Adapter<ChatAdaptor.ChatViewHolder>{
+
+        private final Context context;
+
+        private final ArrayList<String> messages;
+
+        public ChatAdaptor(Context context) {
+            this.context = context;
+            messages = new ArrayList<>(Arrays.asList("m1","m2","m3","m4","m5","m6","m7","m8","m9","m10"));
+        }
+
+        @Override
+        public int getItemCount() {
+            return messages.size();
+        }
+
+        @NonNull
+        @Override
+        public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            int layoutItem = R.layout.message_bubble;
+            LayoutInflater inflater = LayoutInflater.from(context);
+            boolean attachImmedietly = false;
+
+            View view = inflater.inflate(layoutItem, parent, attachImmedietly);
+            ChatViewHolder cv = new ChatViewHolder(view);
+
+            return cv;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+            String m = messages.get(position);
+
+            holder.tv_message.setText(m);
+        }
+
+        public static class ChatViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView tv_message;
+
+            public ChatViewHolder(View itemView) {
+                super(itemView);
+
+                tv_message = itemView.findViewById(R.id.tv_message_bubble);
+
+            }
+
+        }
+
+    }
+
+
+
+
 }
